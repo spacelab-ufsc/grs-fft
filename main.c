d294bc718761aace21911fb8cfa05d397da4d69e
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2025-06-23 21:06:52 -0300

    Initial project structure

---
+++ main.c
@@ -0,0 +1,42 @@
+/*
+ * main.c
+ * 
+ * Copyright The GRS FFT Contributors.
+ * 
+ * This file is part of GRS FFT.
+ * 
+ * GRS FFT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GRS FFT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GRS FFT. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief GRS FFT calculator.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.0.0
+ * 
+ * \date 2025/06/23
+ * 
+ * \defgroup grs-fft GRS FFT
+ * \{
+ */
+
+#include "version.h"
+
+int main(int argc, char *argv[])
+{
+}
+
+/** \} End of grs-fft group */
---
+++ version.h
@@ -0,0 +1,26 @@
+/**
+ * \brief Version control file.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.0.0
+ * 
+ * \date 2025/06/23
+ * 
+ * \defgroup version Version
+ * \ingroup grs-fft
+ * \{
+ */
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define GRS_FFT_VERSION     "0.0.0"
+#define GRS_FFT_STATUS      "Development"
+
+#define GRS_FFT_AUTHORS     {"Gabriel Mariano Marcelino"}
+#define GRS_FFT_EMAILS      {"gabriel.mm8@gmail.com"}
+
+#endif /* VERSION_H_ */
+
+/** \} End of version group */
